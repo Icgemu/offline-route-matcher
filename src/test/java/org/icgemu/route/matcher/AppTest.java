d57dd23e44c843a978ea26153b50fc7fdc940cff
icgemu <yuanrong303@yeah.net>
2017-01-18 16:47:09 +0800

    init

---
+++ src/test/java/org/icgemu/route/matcher/AppTest.java
@@ -0,0 +1,15 @@
+package org.icgemu.route.matcher;
+
+import junit.framework.TestCase;
+
+public class AppTest extends TestCase {
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+}
