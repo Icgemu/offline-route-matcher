c8c08e9e6d2da91f00466cdf85d4709f11bf440e
icgemu <yuanrong303@yeah.net>
2017-01-19 13:41:31 +0800

    add sub module

---
+++ compiler/src/main/java/org/icgemu/route/maptcher/compiler/App.java
@@ -0,0 +1,13 @@
+package org.icgemu.route.maptcher.compiler;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
